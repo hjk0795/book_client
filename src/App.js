184b691fcae1125cf8e4b583e8d95a133bd111e8
hjk0795 <hjk0795@gmail.com>
2022-04-01 18:23:02 +0900

    book frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateBook from "./components/CreateBook";
+import ShowBookList from "./components/ShowBookList";
+import ShowBookDetails from "./components/ShowBookDetails";
+import UpdateBookInfo from "./components/UpdateBookInfo";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<ShowBookList />}></Route>
+            <Route path="/create-book" element={<CreateBook />}></Route>
+            <Route path="/edit-book/:id" element={<UpdateBookInfo />}></Route>
+            <Route path="/show-book/:id" element={<ShowBookDetails />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
